251732725cf3c0da0874de9d736663d912d2ff60
firesquid6 <jdeiss06@gmail.com>
2024-03-21 14:06:16 -0500

    remove setting i when adding header and parameters

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ const request: UserRequest = {
   headers: new Map(),
 };
 
-let response = "";
+// let response = "";
 
 document.getElementById("url")?.addEventListener("input", (e) => {
   request.url = (e.target as HTMLInputElement).value;
@@ -32,13 +32,85 @@ document.getElementById("method")?.addEventListener("change", (e) => {
 
 document.getElementById("send")?.addEventListener("click", () => {
   console.log(request);
-  fetch(request.url, {
-    method: request.method,
-    body: request.method !== "GET" ? request.body : null,
-  })
-    .then((res) => res.text())
-    .then((res) => {
-      response = res;
-      document.getElementById("response")!.innerHTML = response;
-    });
+  // fetch(request.url, {
+  //   method: request.method,
+  //   body: request.method !== "GET" ? request.body : null,
+  // })
+  //   .then((res) => res.text())
+  //   .then((res) => {
+  //     response = res;
+  //     document.getElementById("response")!.innerHTML = response;
+  //   });
+});
+
+document.getElementById("add-param")?.addEventListener("click", () => {
+  addParam();
+});
+
+function addParam() {
+  const i = request.params.size;
+  const newHtml = `
+    <div id="param-${i}">
+      <input type="text" id="p-key-${i}" placeholder="Key">
+      <input type="text" id="p-value-${i}" placeholder="Value">
+      <button id="p-delete-${i}">-</button>
+    </div>
+  `;
+  document.getElementById("params")!.insertAdjacentHTML("beforeend", newHtml);
+
+  let key = "";
+  let value = "";
+  request.params.set(key, value);
+
+  document.getElementById(`p-delete-${i}`)!.addEventListener("click", () => {
+    document.getElementById(`param-${i}`)!.remove();
+    request.params.delete(key);
+  });
+
+  document.getElementById(`p-key-${i}`)!.addEventListener("input", (e) => {
+    request.params.delete(key);
+    key = (e.target as HTMLInputElement).value;
+    request.params.set(key, value);
+  });
+
+  document.getElementById(`p-value-${i}`)!.addEventListener("input", (e) => {
+    value = (e.target as HTMLInputElement).value;
+    request.params.set(key, value);
+  });
+}
+
+function addHeader() {
+  const i = request.headers.size;
+  const newHtml = `
+    <div id="header-${i}">
+      <input type="text" id="h-key-${i}" placeholder="Key">
+      <input type="text" id="h-value-${i}" placeholder="Value">
+      <button id="h-delete-${i}">-</button>
+    </div>
+  `;
+  document.getElementById("headers")!.insertAdjacentHTML("beforeend", newHtml);
+
+  let key = "";
+  let value = "";
+  request.headers.set(key, value);
+
+  document.getElementById(`h-delete-${i}`)!.addEventListener("click", () => {
+    document.getElementById(`header-${i}`)!.remove();
+    request.headers.delete(key);
+  });
+
+  document.getElementById(`h-key-${i}`)!.addEventListener("input", (e) => {
+    request.headers.delete(key);
+    key = (e.target as HTMLInputElement).value;
+    request.headers.set(key, value);
+  });
+
+  document.getElementById(`h-value-${i}`)!.addEventListener("input", (e) => {
+    value = (e.target as HTMLInputElement).value;
+    request.headers.set(key, value);
+  });
+}
+
+document.getElementById("add-header")?.addEventListener("click", () => {
+  addHeader();
 });
